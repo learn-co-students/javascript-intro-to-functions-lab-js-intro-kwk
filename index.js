ca96acc6f2dc53ac44bf7016e4726bf1c4bacf11
Stacey Williams <hello@stacey-williams.com>
2016-08-17 03:38:45 +0000

    Done.

--- index.js
+++ index.js
@@ -15,11 +15,14 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === lowercase) {
-      return "I can\'t hear you!";
-} else if (string === uppercase) {
+  if (string.toLowerCase() === string) {
+      return "I can\'t hear you!"
+  }
+  if (string.toUpperCase() === string) {
      return "YES INDEED!"
-} else if string === "I love you, Grandma." {
-       return "I love you, too.";
-}
+  }
+  if (string === "I love you, Grandma.") {
+       return "I love you, too."
+  }
+
 }
