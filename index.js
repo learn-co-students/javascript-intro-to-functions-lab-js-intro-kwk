a8689ffd19a880986e47fb481432901f813287b9
Branislava Veselinovic <bveselinovic@outlook.com>
2016-09-21 17:09:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+}
+
+function logWhisper(string) {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+  if ('hello')
+  { return ("I can't hear you!")
+}
+  else if ('HELLO')
+  { return ("YES INDEED!")
+}
+  else if( "I love you, Grandma.")
+  { return ("I love you, too.")
+}
+}
