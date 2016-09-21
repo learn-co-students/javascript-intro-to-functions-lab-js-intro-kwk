8b50cd71f590a482b13460530c9c09b3f2082703
Victoria Chiu <victoria@chiucompany.com>
2016-09-21 15:07:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-var uppercase = "HELLO!"
-
-uppercase.toUpperCase() === uppercase // true
-
-var lowercase = "hello!"
-
-lowercase.toLowerCase() === lowercase // true
-
-var mixedCase = "Hi there!"
-
-mixedCase.toLowerCase() === mixedCase // false
-
-mixedCase.toUpperCase() === mixedCase // false
-
 function shout(string) {
   return string.toUpperCase()
 }
@@ -19,10 +5,27 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
 function logShout(string) {
-  return string.toUpperCase("HELLO")
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string) {
-  return string.toLowerCase("hello")
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
 }
