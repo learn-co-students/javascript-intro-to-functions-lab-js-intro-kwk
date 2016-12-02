0eb0d61b745b09129ce294f3d76892d440f8870b
Kimia Kalantari <kimiakalantari1356@gmail.com>
2016-12-02 01:00:32 +0000

    Done.

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if (string === string.toLowerCase()) {
-    return "I can't hear you!"
+    return 'I can\'t hear you!'
   }
 }
 
 function sayHiToGrandma(string){
   if (string === string.toUpperCase()) {
-    return "YES INDEED!"
+    return 'YES INDEED!'
   }
 }
 
