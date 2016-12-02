bc3196f66def1bdb28b6f67c77eb86e5aad4541a
Kimia Kalantari <kimiakalantari1356@gmail.com>
2016-12-02 05:09:41 +0000

    Done.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string === string.toLowerCase()) {
+  if (string == string.toLowerCase()) {
     return 'I can\'t hear you!'
   }
 }
