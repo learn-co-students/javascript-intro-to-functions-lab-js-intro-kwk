184462f77f1ddb810d21ee8cca79da726bd3c7d0
Kimia Kalantari <kimiakalantari1356@gmail.com>
2016-12-02 15:40:36 +0000

    Done.

--- index.js
+++ index.js
@@ -18,15 +18,9 @@ function sayHiToGrandma(string){
   if (string === string.toLowerCase()) {
     return 'I can\'t hear you!';
   }
-}
-
-function sayHiToGrandma(string){
   if (string === string.toUpperCase()) {
     return 'YES INDEED!';
   }
-}
-
-function sayHiToGrandma(string){
   if (string === "I love you, Grandma."){
     return "I love you, too."
   }
