db564a7eaf4c418b2187822346770a40153a11c6
Kimia Kalantari <kimiakalantari1356@gmail.com>
2016-12-02 05:12:04 +0000

    Done.

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string == string.toLowerCase()) {
+  if (string === string.toLowerCase()) {
     return 'I can\'t hear you!'
   }
 }
 
 function sayHiToGrandma(string){
-  if (string === string.toUpperCase()) {
+  if (string === 'string'.toUpperCase()) {
     return 'YES INDEED!'
   }
 }
